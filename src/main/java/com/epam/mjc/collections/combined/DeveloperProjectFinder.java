71b99a82112cbaa5157366c27323dfadbf7657be
kirichenkods <kirichenkods@ya.ru>
2024-04-23 22:17:43 +0300

    add solution

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,29 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> developerProjects = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> e : projects.entrySet()) {
+            if (e.getValue().contains(developer)) {
+                developerProjects.add(e.getKey());
+            }
+        }
+        ListComparator listComparator = new ListComparator();
+        developerProjects.sort(listComparator);
+
+        return developerProjects;
+    }
+}
+
+class ListComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String s1, String s2) {
+        if (s1.length() == s2.length()) {
+            return -(s1.compareTo(s2));
+        }
+        return s2.length() - s1.length();
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,18 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> mapKeys = new HashMap<>();
+        for (Map.Entry<String, Integer> e : sourceMap.entrySet()) {
+            String keySource = e.getKey();
+            int key = keySource.length();
+            Set<String> setKeys = mapKeys.getOrDefault(key, new HashSet<>());
+            setKeys.add(keySource);
+            mapKeys.put(key, setKeys);
+        }
+
+        return mapKeys;
     }
 }
